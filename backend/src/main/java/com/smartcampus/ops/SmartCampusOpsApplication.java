b5cfa2cebe943684103a29cefe81933d596daa5b
NaleeshaThanushka <naleeshathanushka@gmail.com>
2026-04-16 12:32:22 +0530

    update main application class

--- backend/src/main/java/com/smartcampus/ops/SmartCampusOpsApplication.java
+++ backend/src/main/java/com/smartcampus/ops/SmartCampusOpsApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartCampusOpsApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(SmartCampusOpsApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCampusOpsApplication.class, args);
+    }
 }
-
