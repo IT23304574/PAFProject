a42bb16c295f0f7a054e6b7f7b640e3dd95c818c
Amasha Sinhalagoda <amashavs03@gmail.com>
2026-04-24 21:28:22 +0530

    correct package declaration in User.java

---
+++ backend/src/main/java/com/smartcampus/ops/auth/model/User.java
@@ -0,0 +1,19 @@
+package com.smartcampus.ops.auth.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String role; // USER / ADMIN
+}
