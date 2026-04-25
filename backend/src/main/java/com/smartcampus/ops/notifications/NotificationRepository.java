3604169b919d8c5879a7ba4fbf8c9895a79a3b38
Amasha Sinhalagoda <amashavs03@gmail.com>
2026-04-26 03:02:08 +0530

    Removed

--- backend/src/main/java/com/smartcampus/ops/notifications/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcampus.ops.notifications;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-  List<Notification> findByUserIdAndRead(String userId, boolean read);
-  List<Notification> findByUserId(String userId);
-}
